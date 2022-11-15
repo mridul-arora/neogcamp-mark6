6a4a94d1a5479ebe55fccb2fb7c961cc1a89a3cc
MRIDUL ARORA <aroramridul18@gmail.com>
2022-11-15 14:45:03 +0530

    code : errorhandling done for >5 requests

--- translate.js
+++ translate.js
@@ -1,27 +1,35 @@
 var btnTrans = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
+var txtErrorMsg = document.querySelector("#txt-errormsg");
 
  var url = "https://api.funtranslations.com/translate/minion.json"
 //var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 function errorHandler(error){
-    console.log("Error occured.." + error);
-    alert("Server is offline");
+    console.log("Error: " + error);
+    txtErrorMsg.innerHTML = "Error: " + error;
 }
 
 function clickHandler() {
     var text = txtInput.value;
     var parametrizedURL = url + "?" + "text=" + text;
 
-    function divOutput(out){
-        var displayOutput = "Translated text to minion is as... ";
-        txtOutput.innerHTML = displayOutput + out.contents.translated;   
+    function divOutput(out){        
+        if(out.error) {
+                        
+        }else if(out.success){
+            var displayOutput = "Translated text to minion is as... ";
+            txtOutput.innerHTML = displayOutput + out.contents.translated;
+        }        
     }
 
     fetch(parametrizedURL)
     .then(response => response.json())
-    .then(json => divOutput(json))
+    .then(json => {
+        if(json.error) throw json.error.message
+        else divOutput(json)
+    })
     .catch(errorHandler)
 }
 
